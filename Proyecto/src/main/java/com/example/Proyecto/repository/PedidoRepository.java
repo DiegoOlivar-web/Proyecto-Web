c3c198286c99057ac19e8dd9ae0b32f36769b920
AndresRYQ <andresricardoybanezquispe@gmail.com>
2026-06-08 23:49:54 -0500

    prueba fallida

--- Proyecto/src/main/java/com/example/Proyecto/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Proyecto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.Proyecto.Entity.PedidoEntity;
-
-public interface PedidoRepository extends JpaRepository<PedidoEntity, Long> {
-}
